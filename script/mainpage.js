0196fa0c5cdd4f8f46976ed466804d2927c952ed
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-03-08 23:42:46 -0400

    worked on input field/search btn and fetchng data

--- script/mainpage.js
+++ script/mainpage.js
@@ -15,7 +15,7 @@ const modalDes = document.getElementById("modal-des");
 const modalAssignee = document.getElementById("assignee");
 const modalpriority = document.getElementById("priority");
 const loadingSpinner = document.getElementById("loadingSpinner");
-const searchInput = document.getElementById("search-input");
+
 const searchBtn = document.getElementById("search-btn");
 let openIssues = [];
 let closedIssues = [];
@@ -248,6 +248,61 @@ async function openIssueModal(issueid) {
 
 openBtn.addEventListener("click", pullData);
 closedBtn.addEventListener("click", pullClosedData);
+
+// searchBtn.addEventListener("click", () => {
+//   const serachItem = searchInput.value;
+//   console.log(serachItem);
+// });
+
+async function searchInfo() {
+  const inputField = document.getElementById("search-input");
+  const inputValue = inputField.value;
+  // console.log(inputValue);
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${inputValue}`,
+  );
+  const data = await res.json();
+  const onlyData = await data.data;
+  displaySearchInfo(onlyData);
+  countIssue.textContent = onlyData.length;
+}
+
+function displaySearchInfo(issues) {
+  cardContainer.innerHTML = "";
+  issues.forEach((issue) => {
+    const issueCard = document.createElement("div");
+    issueCard.className = `${issue.status == "open" ? "card bg-white border-t border-green-500 p-5 shadow-md space-y-3 hover:cursor-pointer" : "card bg-white border-t border-purple-500 p-5 shadow-md space-y-3 hover:cursor-pointer"}`;
+    issueCard.innerHTML = `
+  
+          <!-- card top part  -->
+          <div class="flex justify-between">
+            <img class="" src="./assets/Open-Status.png" alt="" />
+            <button class="bg-[#FEECEC]  p-2">${issue.priority}</button>
+          </div>
+          <!-- card middle part  -->
+          <div onclick="openIssueModal(${issue.id})">
+            <h3 class="font-bold">${issue.title}</h3>
+            <p class="pb-3">
+              ${issue.description}
+            </p>
+            <button class="bg-yellow-200">${issue.labels?.[0]}</button>
+            <button class="pl-2 bg-yellow-200">${issue.labels?.length === 2 ? issue.labels[1] : ""}</button>
+          </div>
+          <hr class="text-gray-300" />
+          <!-- card bottom part  -->
+          <div class="">
+        
+            <p>#${issue.id} by ${issue.author} &nbsp; &nbsp; ${issue.createdAt} </p>
+             <br />
+            <p>Assignee: ${issue.assignee} &nbsp; &nbsp; updated: ${issue.updatedAt} </p>
+          
+           </div>
+      
+    `;
+
+    cardContainer.appendChild(issueCard);
+  });
+}
 
 showLoading();
 const myTimeout = setTimeout(loadAllIssues, 2000);
